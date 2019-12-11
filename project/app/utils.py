82c3620743aba7a1da66fd9fe049416dabc90501
Renan Penna <victorp.info@yahoo.com.br>
2019-12-11 06:49:46 -0300

    DEV - Update utils.py, with new functions for the project

--- project/app/utils.py
+++ project/app/utils.py
@@ -6,37 +6,36 @@ Neste arquivo, temos as funções recorrentes para todos os clientes.
 import re
 from os.path import dirname, join
 
-# from app.models.core import LogApi, db
+from app.models.core import Logs
 from flask import Response, json
 
 
 def format_string_for_snake(string):
     """Format any string, excluded special character and convert to patterns snake case"""
-    regex = re.compile('[@!#$%^&*()<>?/\|}{~:]')
+    regex = re.compile('[@!#$%^&*()<>?\|}{~:]')
     if(regex.search(string) == None):
-        return string.replace(" ", "_")
+        s = string.replace(" ", "_")
+        return s.replace("/", "_")
 
     else:
         result = regex.sub("", string).casefold().replace(" ", "_")
-        return result
+        return result.replace("/", "_")
+
+# TODO: Retirar contexto
+# def pagination(data, url, start, limit):
+#     count = len(data)
+#     if (count < start):
+#         abort(404)
+#     return dict(
+#         start=start,
+#         limit=limit,
+#         count=count,
+#         message=data[(start - 1):(start - 1 + limit)],
+#     )
 
 
-def pagination(data, url, start, limit):
-    count = len(data)
-    if (count < start):
-        abort(404)
-    return dict(
-        start=start,
-        limit=limit,
-        count=count,
-        message=data[(start - 1):(start - 1 + limit)],
-    )
-
-
-# def register_log(user_id, resource, information=None):
-#     log = LogApi(user_id, resource, information)
-#     db.session.add(log)
-#     db.session.commit()
+def register_log(filename, list_errors):
+    log = LogApi(name_file=filename, errors=list_errors)
 
 
 def send_reponse(msg, code, headers=None):
@@ -49,7 +48,7 @@ def send_reponse(msg, code, headers=None):
         send.headers.add(headers[0], headers[1])
     return send
 
-
-def get_local_env():
-    path = join(dirname(__file__), '.env')
-    return path
+# TODO: Analisar uso e contexto
+# def get_local_env():
+#     path = join(dirname(__file__), '.env')
+#     return path
