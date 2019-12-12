a857b2fb6cfda6552588a2bd265b0c39b729a577
Renan Penna <victorp.info@yahoo.com.br>
2019-12-11 21:26:22 -0300

    DEV - Update schemas.py, refactored file.

--- project/app/schemas.py
+++ project/app/schemas.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
+import numpy
 from dateutil.parser import parse
 
 from marshmallow import Schema, ValidationError, fields, validate
 
 
+def validate_dataset(value):
+    """Function to validate if value is NaN"""
+    if value == numpy.nan or value == 'nan':
+        raise ValidationError("Element is None")
+
+
 class DataSetSchema(Schema):
+    """Schema for dataset"""
     ra_report_ = fields.Int(
         validate=[validate_dataset, validate.Equal(int)])
     ra_caers_created_date = fields.DateTime(
         format="m%-d%-Y%", validate=[
-            validate_dataset,
-            validate_date
+            validate_dataset
         ])
     aec_event_start_date = fields.DateTime(
         format="m%-d%-Y%", validate=[
-            validate_dataset,
-            validate_date
+            validate_dataset
         ])
     pri_product_role = fields.String(
         validate=[validate_dataset, validate.Equal(str)])
@@ -37,12 +43,3 @@ class DataSetSchema(Schema):
         validate=[validate_dataset, validate.Equal(str)])
     sym_one_row_coded_symptoms = fields.String(
         validate=[validate_dataset, validate.Equal(str)])
-
-    def validate_dataset(n):
-        if n == 'nan':
-            raise ValidationError("Element is None")
-
-# TODO: Analisar utilidade
-    # def validate_date(n):
-    #     if datetime(parse(n)):
-    #         raise ValidationError('Element is not date')
